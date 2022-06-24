f07f99bfe2c93a47a61f8a2012c4de8214b082f0
haha505254 <haha505254@gmail.com>
2022-06-24 17:08:58 +0800

    modify_homepage_gitignore

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 import datetime
 
+
 def home(request):
-    now = datetime.datetime.now() # 現在時間
-    context = {'now':now}
+    now = datetime.datetime.now()  # 現在時間
+    context = {'now': now}
     return render(request, 'home.html', context)
