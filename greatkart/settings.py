e00f2e31a8295b87a70d4d5daee08e9ce2097e4c
haha505254 <haha505254@gmail.com>
2022-06-30 13:39:36 +0800

    add static media & category slug

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -125,7 +125,8 @@ STATICFILES_DIRS = [
     'greatkart/static'
 ]
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- readme.txt
+++ readme.txt
@@ -2,3 +2,4 @@ Email: admin@admin.com
 Username: admin
 First name: admin
 Last name: admin
+password:123456
