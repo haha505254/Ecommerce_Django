f2c14d77150564cb0239c8bf16f014056bcf5ee4
haha505254 <haha505254@gmail.com>
2022-07-19 20:07:15 +0800

    dynamic carts icon

--- store/views.py
+++ store/views.py
@@ -1,9 +1,14 @@
+from http.client import HTTPResponse
+from itertools import product
+from math import prod
 from unicodedata import category
 from django.shortcuts import get_object_or_404, render
 
 from category.models import Category
-
+from carts.views import _cart_id
+from carts.models import CartItem
 from .models import Product
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -28,9 +33,11 @@ def store(request, category_slug=None):
 def product_detail(request,category_slug,product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug,slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request),product=single_product).exists()
     except Exception as e:
         raise e
     context = {
         'single_product':single_product,
+        'in_cart':in_cart,
     }
     return render(request, 'store/product_detail.html',context)
